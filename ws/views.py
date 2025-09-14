aa9e1f70bf44588d820bd9a5b054fc2be3e4b4a8
M B SURYANARAYANAN <mbsurya19122004@gmail.com>
2025-09-14 17:57:12 +0530

    fixed radii

--- ws/views.py
+++ ws/views.py
@@ -11,7 +11,7 @@ class RegionListView(APIView):
                 "identifier": "Lachen",
                 "latitude": 27.733333,
                 "longitude": 88.55,
-                "radius": 50000,
+                "radius": 5000,
                 "notifyOnEnter": True,
                 "notifyOnExit": True,
                 "entered": False
@@ -20,7 +20,7 @@ class RegionListView(APIView):
                 "identifier": "Yumthang Valley, Yumthang",
                 "latitude": 27.825926,
                 "longitude": 88.695831,
-                "radius": 50000,
+                "radius": 5000,
                 "notifyOnEnter": True,
                 "notifyOnExit": True,
                 "entered": False
@@ -29,7 +29,7 @@ class RegionListView(APIView):
                 "identifier": "Chungthang",
                 "latitude": 27.605,
                 "longitude": 88.609,
-                "radius": 50000,
+                "radius": 5000,
                 "notifyOnEnter": True,
                 "notifyOnExit": True,
                 "entered": False
@@ -38,7 +38,7 @@ class RegionListView(APIView):
                 "identifier": "Noney (NH-37) Noney",
                 "latitude": 24.859,
                 "longitude": 93.633,
-                "radius": 50000,
+                "radius": 5000,
                 "notifyOnEnter": True,
                 "notifyOnExit": True,
                 "entered": False
@@ -47,7 +47,7 @@ class RegionListView(APIView):
                 "identifier": "Barak Valley",
                 "latitude": 24.833,
                 "longitude": 92.7789,
-                "radius": 50000,
+                "radius": 5000,
                 "notifyOnEnter": True,
                 "notifyOnExit": True,
                 "entered": False
@@ -56,7 +56,7 @@ class RegionListView(APIView):
                 "identifier": "Kohima (NH-2)",
                 "latitude": 25.634,
                 "longitude": 94.098,
-                "radius": 50000,
+                "radius": 5000,
                 "notifyOnEnter": True,
                 "notifyOnExit": True,
                 "entered": False
@@ -65,7 +65,7 @@ class RegionListView(APIView):
                 "identifier": "Shillong",
                 "latitude": 25.2,
                 "longitude": 92.033333,
-                "radius": 50000,
+                "radius": 5000,
                 "notifyOnEnter": True,
                 "notifyOnExit": True,
                 "entered": False
@@ -74,7 +74,7 @@ class RegionListView(APIView):
                 "identifier": "Itanagar Hills",
                 "latitude": 27.1,
                 "longitude": 93.616667,
-                "radius": 50000,
+                "radius": 5000,
                 "notifyOnEnter": True,
                 "notifyOnExit": True,
                 "entered": False
